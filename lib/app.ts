aacb586827665db69bd2c6df844affeafb78154a
Florian Rand <florian.rand@gmail.com>
2019-11-27 21:37:41 +0100

    Remove db config

--- lib/app.ts
+++ lib/app.ts
@@ -1,28 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import bluebird from "bluebird";
 
 const app = express();
-const mongoUrl = process.env.MONGODB_URI || process.env.MONGODB_LOCAL_URI;
-mongoose.Promise = bluebird;
-
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
-  })
-  .catch(err => {
-    console.error(
-      `MongoDB connection error. Please make sure MongoDB is running. ${err}`,
-    );
-    // process.exit();
-  });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
