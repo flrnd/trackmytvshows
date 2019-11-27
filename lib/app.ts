53d4e5fc0af23a9df8a73033cb5fbdc5726ec607
Florian Rand <florian.rand@gmail.com>
2019-11-27 21:26:19 +0100

    Refactor

--- lib/app.ts
+++ lib/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import bluebird from "bluebird";
@@ -23,7 +23,6 @@ mongoose
     // process.exit();
   });
 
-app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,11 @@
 import errorHandler from "errorhandler";
 import app from "./app";
 
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
