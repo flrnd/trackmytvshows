a483b14d2e729030617316718c0c0bc1cd0068a0
Florian Rand <florian.rand@gmail.com>
2019-11-27 20:31:38 +0100

    App server and controller

--- lib/app.ts
+++ lib/app.ts
@@ -1,18 +1,30 @@
-import express, { Application } from 'express';
-import bodyParser from 'body-parser';
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import bluebird from "bluebird";
 
-class App {
-  public app: Application;
+const app = express();
+const mongoUrl = process.env.MONGODB_URI || process.env.MONGODB_LOCAL_URI;
+mongoose.Promise = bluebird;
 
-  constructor() {
-    this.app = express();
-    this.config();
-  }
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch(err => {
+    console.error(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`,
+    );
+    // process.exit();
+  });
 
-  private config(): void {
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-  }
-}
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-export default new App().app;
+export default app;
--- lib/server.ts
+++ lib/server.ts
@@ -1,4 +1,5 @@
-import app from './app';
+import errorHandler from "errorhandler";
+import app from "./app";
 
 const PORT = process.env.PORT;
 
