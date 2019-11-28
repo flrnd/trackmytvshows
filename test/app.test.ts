4163ab44cf81808b9ec80952f074484d949f474e
Florian Rand <florian.rand@gmail.com>
2019-11-28 17:07:22 +0100

    Add /api/test

--- test/app.test.ts
+++ test/app.test.ts
@@ -2,9 +2,17 @@ import request from "supertest";
 import app from "../lib/app";
 
 describe("GET /", () => {
-  it("should return 404", done => {
+  it("should return 200", done => {
     request(app)
       .get("/")
-      .expect(404, done);
+      .expect(200, done);
+  });
+});
+
+describe("GET /api/", () => {
+  it("should return 401 Unauthorized", done => {
+    request(app)
+      .get("/api/")
+      .expect(401, done);
   });
 });
