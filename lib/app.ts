4c50942adb36eb6c947a52416c5502371971e6c2
Florian Rand <florian.rand@gmail.com>
2019-11-28 16:56:25 +0100

    Add auth0 brearer token

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
-import Database from "./config/database";
+import jwt from "express-jwt";
+import jwksRsa from "jwks-rsa";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Auth
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: process.env.AUTH0_JWKS_URI,
+  }),
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: process.env.AUTH0_ISSUER,
+  algorithms: ["RS256"],
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Track my TV Shows!",
+  });
+});
+
+app.get("/api/", checkJwt, (req, res) => {
+  res.json({ message: "Hello from private endpoint!" });
+});
 export default app;
