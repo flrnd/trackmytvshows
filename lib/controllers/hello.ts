b3b30caac730c276e9762f294b259b49ecd825bc
Florian Rand <florian.rand@gmail.com>
2019-12-03 12:56:23 +0100

    Add root hello controller

---
+++ lib/controllers/hello.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const getHello = (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Hello from Track my TV Shows!",
+  });
+};
+
+export const putHello = (req: Request, res: Response) => {
+  req.body.name
+    ? res.status(200).json({ message: `Hello ${req.body.name}!` })
+    : res.status(401).json({ message: "Hello stranger!" });
+};
