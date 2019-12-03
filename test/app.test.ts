e147fffd60b2a8607437bd0dbdeb807e4843f28d
Florian Rand <florian.rand@gmail.com>
2019-12-03 17:49:51 +0100

    Add 404 test

--- test/app.test.ts
+++ test/app.test.ts
@@ -8,6 +8,13 @@ describe("GET /", () => {
       .expect(200, done);
   });
 });
+describe("GET /api/", () => {
+  it("should return 404 Not Found", done => {
+    request(app)
+      .get("/api/")
+      .expect(404, done);
+  });
+});
 
 describe("GET /api/tvshow/", () => {
   it("should return 401 Unauthorized", done => {
