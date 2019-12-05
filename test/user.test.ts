f72c8cf6dcd8d95b951c895a87a5cbe3494e4eaa
Florian Rand <florian.rand@gmail.com>
2019-12-05 20:23:47 +0100

    Add User model tests cases

---
+++ test/user.test.ts
@@ -0,0 +1,87 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { User } from "../lib/models/User";
+import * as dummyData from "./dummyData/users";
+import { MongoError } from "mongodb";
+
+describe("User model tests", () => {
+  beforeAll(async () => {
+    const databaseURL = process.env.MONGO_URL;
+    await mongoose.connect(databaseURL, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+  });
+
+  it("Should create and save a new user", async () => {
+    const validUser = new User(dummyData.user);
+    const savedUser = await validUser.save();
+    const password = dummyData.user.password;
+    const savedPassword = savedUser.password;
+    const compare = bcrypt.compare;
+
+    expect(savedUser._id).toBeDefined();
+    expect(savedUser.email).toBe(dummyData.user.email);
+    expect(await compare(password, savedPassword)).toBe(true);
+  });
+
+  it("should fail with blank fields", async () => {
+    const blankUser = new User(dummyData.blankUser);
+    try {
+      await blankUser.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+    }
+  });
+
+  it("should fail with blank email", async () => {
+    const blankEmail = new User(dummyData.blankEmail);
+    try {
+      await blankEmail.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+    }
+  });
+
+  it("should fail with blank password", async () => {
+    const blankPassword = new User(dummyData.blankPassword);
+    try {
+      await blankPassword.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+    }
+  });
+
+  it("should fail with wrong email", async () => {
+    const wrongEmail = new User(dummyData.wrongEmail);
+    try {
+      await wrongEmail.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+    }
+  });
+
+  it("should fail with short password", async () => {
+    const shortPassword = new User(dummyData.shortPassword);
+    try {
+      await shortPassword.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
+    }
+  });
+
+  it("should fail when try to save the same user", async () => {
+    const existingUser = new User(dummyData.user);
+    try {
+      await existingUser.save();
+    } catch (error) {
+      expect(error).toBeInstanceOf(MongoError);
+    }
+  });
+});
