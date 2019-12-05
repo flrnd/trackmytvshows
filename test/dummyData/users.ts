8874332cbb7d3a1df8daea1a47d33c45769d6d03
Florian Rand <florian.rand@gmail.com>
2019-12-05 20:24:09 +0100

    Testing dummy data

---
+++ test/dummyData/users.ts
@@ -0,0 +1,29 @@
+export const user = {
+  email: "some@email.com",
+  password: "12345678",
+};
+
+export const wrongEmail = {
+  email: "some@",
+  password: "12345678",
+};
+
+export const blankEmail = {
+  email: "",
+  password: "12345678",
+};
+
+export const blankUser = {
+  email: "",
+  password: "",
+};
+
+export const shortPassword = {
+  email: "somerandom@email.com",
+  password: "short",
+};
+
+export const blankPassword = {
+  email: "some@email.com",
+  password: "",
+};
