08e94311d3584930451d7d472049f0ee73c3cb49
Florian Rand <florian.rand@gmail.com>
2019-12-05 15:35:26 +0100

    Refactor

--- lib/controllers/user.ts
+++ lib/controllers/user.ts
@@ -12,45 +12,24 @@ export const postLogin = async (
   res: Response,
   next: NextFunction,
 ) => {
-  await check("email", "Email is not valid")
-    .isEmail()
-    .run(req);
-  await check("password", "Password cannot be blank or less than 8 characters")
-    .isLength({ min: 8 })
-    .run(req);
-  await sanitize("email")
-    .normalizeEmail({ gmail_remove_dots: false })
-    .run(req);
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(500).send({ Errors: errors.array() });
-  }
-
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
-    const existingUser = await checkUser(user.email);
+    await validateEmailPassword(req);
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const existingUser = await findUser(user.email);
     if (!existingUser) {
-      return res.status(401).send({ message: "Wrong email." });
+      throw new Error("Wrong email.");
     }
-    const match = await validateUserPassword(
-      user.password,
-      existingUser.password,
-    );
-    if (!match) {
-      return res.status(401).send({ auth: false, token: null });
-    }
+    await validateUserPassword(user.password, existingUser.password);
     const token = jwt.sign({ id: existingUser._id }, jwtSecret, {
       expiresIn: 86400,
     });
     res.status(200).send({ auth: true, token });
   } catch (error) {
-    console.error(error.message);
+    //console.error(error.message);
+    return res.status(401).send({ error: `Login failed. ${error}` });
   }
 };
 
@@ -59,47 +38,47 @@ export const postSignup = async (
   res: Response,
   next: NextFunction,
 ) => {
-  await check("email", "Email is not valid")
-    .isEmail()
-    .run(req);
-  await check("password", "Password cannot be blank")
-    .isLength({ min: 8 })
-    .run(req);
-  await sanitize("email")
-    .normalizeEmail({ gmail_remove_dots: false })
-    .run(req);
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(500).send({ Errors: errors.array() });
-  }
-
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
-    const existingUser = await checkUser(user.email);
+    await validateEmailPassword(req);
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const existingUser = await findUser(user.email);
     if (existingUser) {
-      res.status(500).send({ message: `${user.email} already in use.` });
+      throw new Error(`${user.email} already in use.`);
     }
     const newUser = await user.save();
     res.status(200).send({ message: `Signed in with ${newUser.email}!` });
   } catch (error) {
-    console.error(error.message);
+    return res.status(403).send({ error: `Signup failed. ${error}` });
   }
 };
 
 const validateUserPassword = async (password: string, hash: string) => {
   const match = await bcrypt.compare(password, hash);
+  if (!match) throw new Error("Password doesn't match.");
   return match;
 };
 
-const checkUser = async (email: string) => {
+const findUser = async (email: string) => {
   const user = await User.findOne({
     email: email.toLowerCase(),
   }).exec();
   return user;
 };
+
+async function validateEmailPassword(req) {
+  await check("email", "Email is not valid")
+    .isEmail()
+    .run(req);
+  await check("password", "Password cannot be blank or less than 8 characters.")
+    .isLength({ min: 8 })
+    .run(req);
+  await sanitize("email")
+    .normalizeEmail({ gmail_remove_dots: false })
+    .run(req);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) throw new Error(`Validation Error: ${errors.array()}`);
+  return errors;
+}
