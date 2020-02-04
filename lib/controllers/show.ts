8b1ab03552abad46e11116b07ef26b0fb7e82d61
Florian Rand <florian.rand@gmail.com>
2020-02-04 18:36:03 +0100

    Refactor getShow and adds putShow

--- lib/controllers/show.ts
+++ lib/controllers/show.ts
@@ -1,14 +1,10 @@
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 import { Show, ShowDocument } from "../models/Show";
 
 export const getShow = (req: Request, res: Response) => {
   const showID = req.params.showID;
   Show.findById(showID)
-    .then(show =>
-      show
-        ? res.status(200).send({ tvshow: show })
-        : res.status(404).send({ tvshow: "Not found." }),
-    )
+    .then(show => res.status(200).send({ tvshow: show }))
     .catch(error => res.status(500).send({ error: error }));
 };
 
@@ -28,6 +24,27 @@ export const postShow = (req: Request, res: Response) => {
     .catch(error => {
       res.status(500).send({ error: error.message });
     });
+};
+
+export const putShow = (req: Request, res: Response) => {
+  const showID = req.params.showID;
+  const updatedShow = {
+    title: req.body.title,
+    url: req.body.url,
+    genre: req.body.genre,
+    air: req.body.air,
+  };
+
+  Show.findById(showID)
+    .then(show => {
+      show.title = updatedShow.title;
+      show.air = updatedShow.air;
+      show.url = updatedShow.url;
+      show.genre = updatedShow.genre;
+      show.save();
+    })
+    .then(response => res.status(200).send({ tvshow: response }))
+    .catch(error => res.status(500).send({ error: error.message }));
 };
 
 const parseShow = (body: any): ShowDocument =>
