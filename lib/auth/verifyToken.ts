67911953577b0991c95fdbacd13338678b69f435
Florian Rand <florian.rand@gmail.com>
2020-02-05 12:32:01 +0100

    cleanup console.log

--- lib/auth/verifyToken.ts
+++ lib/auth/verifyToken.ts
@@ -14,7 +14,6 @@ export const verifyToken = (
     jwt.verify(token, jwtSecret);
     next();
   } catch (error) {
-    //console.error(`Error: Can't verify token: ${error.message}`);
     return res
       .status(403)
       .send({ auth: false, error: `Can't verify token: ${error.message}` });
